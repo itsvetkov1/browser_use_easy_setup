8fe4e7d9801319a273f38b3dfeeb853ef490bb72
itsvetkov1 <i.b.cvetkov@gmail.com>
2025-01-26 17:32:03 +0200

    Simple setup of the browser_use project

--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
 @echo off
+cd %~dp0
+python -V || echo Please install Python 3.11 first!
 python -m venv venv-py311
-call venv-py311\Scripts\activate
+call venv-py311\Scripts\activate.bat
 pip install -r requirements.txt
 playwright install
+echo Setup complete! Virtual environment is activated.
 echo "Next step: Create a .env file with 'OPENAI_API_KEY=your_key_here'"
+pause
